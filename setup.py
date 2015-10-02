5587ed7611422f5509ad16d8876d26e80c4a550f
Tom Wright <tom@maladmin.com>
2015-10-02 18:23:56 -0400

    Updated setup.py with dependencies

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 config = {
     'description': 'An AO registration app based on MATLAB code by S.Burns',
     'author': 'Tom Wright',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
+    'url': 'https://github.com/tomwright01/AO_Registration',
+    'download_url': 'https://github.com/tomwright01/AO_Registration',
     'author_email': 'tom@maladmin.com',
     'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': ['AoRegistration'],
+    'install_requires': ['nose','cv2','numpy','scipy'],
+    'packages': ['AoRegistration','StackTools','ImageTools','tests'],
     'scripts': [],
     'name': 'AoRegistration'
 }
